e8dca0f9c4995bf8ebb316a8c5140367ec815606
STE1NAR <stpr15ab@student.cbs.dk>
2016-12-12 18:21:01 +0100

    Ændret i log ud funktionen

--- src/ServiceImplementation/ServiceImplementation.java
+++ src/ServiceImplementation/ServiceImplementation.java
@@ -900,7 +900,7 @@ public class ServiceImplementation {
 
             int rowsAffected = clearSessionsSQL.executeUpdate();
 
-            if (rowsAffected == 1)  {
+            if (rowsAffected != 0)  {
                 return true;
             }
 
